257efab103e1e164a355419c0ce11ac6b935563c
Davide Mazzaccara <davide.mazzaccara@unitn.it>
2025-08-28 10:52:04 +0200

    Update utils.py

--- DPO/utils.py
+++ DPO/utils.py
@@ -25,3 +25,5 @@ top_10_models_new_clembench = [
  'gpt-4o-2024-08-06-t0.0--gpt-4o-2024-08-06-t0.0',
  'claude-3-opus-20240229-t0.0--claude-3-opus-20240229-t0.0'
 ]
+
+prompt_dpo_turn_wordle_no_explanation = """You are a language wizard who likes to guess words by using the given rules.\n\nWelcome to Wordle! You have six attempts to guess the target word, a valid English word of five lowercase letters (a-z). Please use the tags "guess:".\n\nFor instance, if your guess is "apple", your response should be\nguess: apple.\n\nAfter each guess, your answer will be validated, and you will receive feedback indicating which letters are correct (green), which letters are correct but in the wrong position (yellow), and which letters are incorrect (red). This feedback can be useful in determining which letters to include or exclude in your next guess.\n\nFor example, the feedback for "apple" might be:\nguess_feedback: a<yellow> p<yellow> p<green> l<yellow> e<red>\n\nLet\'s begin with your first guess."""
